d48312c439aa6a7545e3c168f2a21b90783c99a4
afterbvrner <maksim.golish2002@gmail.com>
2020-11-25 12:59:25 +0300

    feat: Logging

--- src/main/java/creator/Fat32Creator.java
+++ src/main/java/creator/Fat32Creator.java
@@ -6,6 +6,7 @@ import component.PrimitiveComponent;
 import lombok.Builder;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,8 @@ import java.io.IOException;
 public class Fat32Creator extends PrimitiveComponent {
     private final BootSector bootSector;
     private final ClusterChain clusterChain;
+
+    private final Logger log = Logger.getLogger(Fat32Creator.class);
 
     @Builder
     public Fat32Creator(int size, int sectorSize, int clusterSize) {
@@ -31,15 +34,20 @@ public class Fat32Creator extends PrimitiveComponent {
 
     public void assembly(String filename) throws IOException {
         byte[] boot = bootSector.getBuffer().array();
+        log.info("Boot sector created");
         byte[] allocation = clusterChain.getAllocationTable().toBytes();
+        log.info("Allocation table loaded");
+        log.info("Converting cluster chain...");
         byte[] clusters = clusterChain.toBytes();
+        log.info("Cluster chain created");
         byte[] fat = ArrayUtils.addAll(
-                ArrayUtils.addAll(
-                        boot, allocation),
+                ArrayUtils.addAll(boot, allocation),
                 clusters);
+        log.info("Creating file \"" + filename + "\"");
         File file = new File(filename);
         if(!file.createNewFile())
             throw new IOException("File creation error");
         FileUtils.writeByteArrayToFile(file, fat);
+        log.info("Successfully created disk image \"" + filename + "\"");
     }
 }
